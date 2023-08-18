80a41457e5b91b8faa9695443407d5a4859f1d37
razberry <razvan.dumitru97@gmail.com>
2023-08-18 18:32:00 +0200

    initial commit

---
+++ arduino_sketch/arduino_sketch.ino
@@ -0,0 +1,112 @@
+#include <Mouse.h>
+#include <Keyboard.h>
+
+#define HORZ_PIN           21         // VRY Joystick
+#define VERT_PIN           20         // VRX Joystick
+#define DEADZONE           50
+#define DESIRED_MILLIS     400
+#define SENSITIVITY        0.005
+#define DT                15          // DT Encoder                      
+#define CLK               18          // CLK Encoder        
+#define joyButt           19         // SW Joystick
+#define button1           2          // Rear Button
+#define button2           3          // Rear Button    
+#define button3           4          // Rear Button    
+#define button4           7          // Rear Button    
+#define button5           8          // Rear Button    
+#define button6           14          // Rear Button    
+#define button7           16          // Rear Button    
+
+int YZero = 0, XZero = 0;     
+
+int YMax = 0, YMin = 0; 
+int XMax = 0, XMin = 0; 
+
+void setup(){
+    pinMode (HORZ_PIN, INPUT_PULLUP);        
+    pinMode (VERT_PIN, INPUT_PULLUP);     
+
+    pinMode (button1, INPUT_PULLUP);
+    pinMode (button2, INPUT_PULLUP);
+    pinMode (button3, INPUT_PULLUP);   
+    pinMode (button4, INPUT_PULLUP);
+    pinMode (button5, INPUT_PULLUP);      
+    pinMode (button6, INPUT_PULLUP);
+    pinMode (button7, INPUT_PULLUP);      
+    pinMode (joyButt, INPUT_PULLUP);  
+    pinMode (DT, INPUT_PULLUP);
+    pinMode (CLK, INPUT_PULLUP);
+    
+    
+    YZero = analogRead(VERT_PIN);  
+    XZero = analogRead(HORZ_PIN);  
+}
+
+bool isPressing = false;
+unsigned long lastMillis = 0;
+
+#define debugValue(display, value) Serial.print(",\""); Serial.print(display); Serial.print("\":"); Serial.print(value);
+#define debugValueWithoutComma(display, value) Serial.print("\""); Serial.print(display); Serial.print("\":"); Serial.print(value);
+
+void loop(){
+  int YValue = analogRead(VERT_PIN) - YZero;  
+  int XValue = analogRead(HORZ_PIN) - XZero;
+
+  bool button1Val = !digitalRead(button1);
+  bool button2Val = !digitalRead(button2); 
+  bool button3Val = !digitalRead(button3); 
+  bool joyButtVal = !digitalRead(joyButt);
+  bool button4Val = !digitalRead(button4); 
+  bool button5Val = !digitalRead(button5); 
+  bool button6Val = digitalRead(button6); 
+  bool button7Val = digitalRead(button7); 
+
+  Serial.print("{");
+
+  debugValueWithoutComma("YValue", YValue);
+  debugValue("XValue", XValue);
+  debugValue("button1", button1Val);
+  debugValue("button2", button2Val);
+  debugValue("button3", button3Val);
+  debugValue("joyButt", joyButtVal);
+  debugValue("scroll", scroll());
+  debugValue("button4", button4Val);
+  debugValue("button5", button5Val);
+  debugValue("button6", button6Val);
+  debugValue("button7", button7Val);
+
+  Serial.println("}");
+
+  delay(10);
+}
+
+bool rot = HIGH;
+bool timeoff = LOW;
+
+uint8_t scroll (void)
+{
+  uint8_t ret = 0;
+  bool scr = digitalRead(DT);
+  bool scr2 = digitalRead(CLK);
+  uint16_t timS;
+  uint16_t hS = 0;
+  timS = millis() - hS;
+  if(scr != scr2)
+    {
+      timeoff = LOW;
+    }
+  if (timS > 10)
+    {
+    hS = millis();
+    if(rot == HIGH)
+      {
+        ret = scr-scr2;
+        rot=!rot; 
+      }
+    if(scr==HIGH && scr2==HIGH)
+      {
+        rot=!rot; 
+      }
+  }
+  return ret;
+}
---
+++ set_cursor_pos.py
@@ -0,0 +1,105 @@
+import win32api
+import datetime
+import serial
+import json
+from pynput.keyboard import Key, Controller
+from pynput.mouse import Button, Controller as MouseController
+
+ser = serial.Serial("COM7", 9600)
+
+#todo get window by id e capire do've il centro
+discard = ser.readline()
+keyboard = Controller()
+mouse = MouseController()
+
+
+def deadzoning(val):
+    if abs(val) < 10:
+        return 0
+    return val
+
+def coordinatePipeline(val):
+    val = deadzoning(val)
+    val /= 100
+    return val
+
+i = 0
+
+squareLato = 100
+
+isPressing = False
+
+lastPress = None
+
+def pressKeys():
+    global lastPress
+    global isPressing
+    currentTime = datetime.datetime.now()
+    if lastPress != None:
+        delta = lastPress - currentTime
+        if delta.microseconds < 500:
+            return
+    keyboard.press(Key.shift)
+    mouse.press(Button.middle)
+    keyboard.press(Key.ctrl_l)
+    isPressing = True
+
+def releaseKeys():
+    global lastPress
+    global isPressing
+    keyboard.release(Key.shift)
+    mouse.release(Button.middle)
+    keyboard.release(Key.ctrl_l)
+    lastPress = datetime.datetime.now()
+    isPressing = False
+
+
+button6Modular = 0
+button7Modular = 0
+
+while True:
+    line = ser.readline().decode('utf-8')
+    dic = json.loads(line)
+    print( "%d, %d" % (dic['YValue'], dic['XValue']))
+
+    xVal = dic['XValue']
+    yVal = dic['YValue']
+
+    xVal = coordinatePipeline(xVal)
+    yVal = coordinatePipeline(yVal)
+
+    if yVal != 0 or xVal != 0:
+
+        if isPressing == False:
+            pressKeys()
+
+        xmPos,ymPos = win32api.GetCursorPos()
+
+        if (xmPos < 1000 - squareLato or xmPos > 1000 + squareLato or 
+            ymPos < 500 - squareLato or ymPos > 500 + squareLato):
+            xmPos = 1000
+            ymPos = 500
+            releaseKeys()
+            win32api.SetCursorPos((xmPos - int(xVal), ymPos + int(yVal)))
+            pressKeys()
+        else:
+            win32api.SetCursorPos((xmPos - int(xVal), ymPos + int(yVal)))
+
+    elif isPressing:
+        releaseKeys()
+
+    button6 = dic['button6']
+
+    if button6 == 1:
+        button6Modular = button6Modular + 1
+        if button6Modular % 6 == 0:
+            mouse.scroll(1,0)
+    
+    button7 = dic['button7']
+
+    if button7 == 1:
+        button7Modular = button7Modular + 1
+        if button7Modular % 6 == 0:
+            mouse.scroll(-1,0)
+    
+    button4 = dic['button4']
